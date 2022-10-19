69f45b69203ace294d543bcc35904a7efc1f9a55
Vlad <6847864@gmail.com>
2022-10-19 22:19:18 +0300

    create repository

---
+++ js/index.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+  $('.header__slider').slick({
+    infinite: true,
+    speed: 500,
+    fade: true,
+    prevArrow:
+      '<img class="arrow-slider arrow-prev" src="images/arrow-right.svg" alt="" />',
+    nextArrow:
+      '<img class="arrow-slider arrow-next" src="images/arrow-right.svg" alt="" />',
+    asNavFor: '.slider-dots',
+  });
+
+  $('.slider-dots').slick({
+    infinite: false,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    asNavFor: '.header__slider',
+    rows: 1,
+    variableWidth: true,
+  });
+});
